07c8226263ed281238d100d85efd01395dadc97c
LudaLutsenko <146946994+LudaLutsenko@users.noreply.github.com>
2024-04-11 13:41:24 +0100

    displayed current weather

--- src/index.js
+++ src/index.js
@@ -66,8 +66,6 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
-
   let apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
