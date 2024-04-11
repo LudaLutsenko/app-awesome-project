cf4519685998b965588a852845e3ea6827f50424
LudaLutsenko <146946994+LudaLutsenko@users.noreply.github.com>
2024-04-11 12:07:34 +0100

    fixed time

--- src/index.js
+++ src/index.js
@@ -3,30 +3,25 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = "0$[hours]";
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = "0$[minutes]";
+    minutes = `0${minutes}`;
   }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+
+
+  let days = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
   let day = days[date.getDay()];
+
   return `${day} ${hours}:${minutes}`;
 }
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let day = date.getDay();
+  let dayIndex = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return days[day];
+  return days[dayIndex];
 }
 
 //Display Forecast days
@@ -35,7 +30,7 @@ function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
+  forecast.forEach (function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
